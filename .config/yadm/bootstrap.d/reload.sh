3b559d909e440a9f1a465f5e8f3229daf6d2cd6f
deviant <48874057+DeviantSec@users.noreply.github.com>
2021-10-18 18:46:15 -0600

    Update reload.sh

--- .config/yadm/bootstrap.d/reload.sh
+++ .config/yadm/bootstrap.d/reload.sh
@@ -2,7 +2,3 @@
 
 # Reloading tmux but it likes to be difficult so we have to open a session in the bg first
 tmux new-session -s 'Session1' -d
-tmux source-file "$HOME/.config/tmux"
-
-# Reload bash
-exec bash
