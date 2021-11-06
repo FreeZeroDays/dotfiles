031ff485abf78bb232729c1d4bb5ab5c6c0f845e
deviant <48874057+DeviantSec@users.noreply.github.com>
2021-11-05 21:22:44 -0600

    Create oh-my-zsh.sh

---
+++ .config/yadm/bootstrap.d/oh-my-zsh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOTS_THEME_FILE="$HOME/.config/oh-my-zsh/deviant.zsh-theme"
+OHMYZSH_THEME_FILE="$HOME/.oh-my-zsh/custom/themes/deviant.zsh-theme"
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    [[ -f ".zshrc.pre-oh-my-zsh" ]] && mv ".zshrc.pre-oh-my-zsh" ".zshrc"
+fi
+
+[ -L "$OHMYZSH_THEME_FILE" ] && rm "$OHMYZSH_THEME_FILE"
+ln -s "$DOTS_THEME_FILE" "$OHMYZSH_THEME_FILE"
+
+echo "Oh My Zsh ✅"
